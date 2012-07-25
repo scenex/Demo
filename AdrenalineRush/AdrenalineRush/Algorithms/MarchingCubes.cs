cd59badb77ad1b90d96a26ff2296ee8f085ab216
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2012-07-25 21:58:53 +0200

    Algorithms folder with no algorithms inside yet.

---
+++ AdrenalineRush/AdrenalineRush/Algorithms/MarchingCubes.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AdrenalineRush.Algorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class MarchingCubes
+    {
+    }
+}
---
+++ AdrenalineRush/AdrenalineRush/Algorithms/Metaball.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Metaball.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AdrenalineRush.Algorithms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Metaball
+    {
+        public Metaball()
+        {
+            
+        }
+
+        public int Calculate(int x, int y, int z, int x0, int y0, int z0)
+        {
+            return 1 / (((x - x0) * (x - x0)) + ((y - y0) * (y - y0)) + ((z - z0) * (z - z0)));
+        }
+    }
+}
