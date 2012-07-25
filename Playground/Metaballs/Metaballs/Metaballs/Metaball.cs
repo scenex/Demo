ed18cd1456c111a73edffc7b53ff2c7cd6fc93b2
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2012-07-25 21:50:30 +0200

    Added "Playground" folder for putting stuff inside that are not yet integrated into the demo. Metaballs 2D implementation added. -> Lift up to marching cube powered 3D implementation.

---
+++ Playground/Metaballs/Metaballs/Metaballs/Metaball.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Metaball.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Metaballs
+{
+    public class Metaball
+    {
+        public uint CenterX { get; set; }
+
+        public uint CenterY { get; set; }
+
+        public uint Radius { get; set; }
+    }
+}
