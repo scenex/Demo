65b75a1fe65d51f72188086e53e46621f63cd0a5
Thomas Kudlacek <thomas_kudlacek@hotmail.com>
2012-07-28 11:48:05 +0200

    Marching cube algorithm ported to C# -> thorough testing needed now... compilable doesn't mean that it works the way it should..

---
+++ Playground/Metaballs/Metaballs/Metaballs/GridCell.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GridCell.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Metaballs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class GridCell
+    {
+        public Vector3[] p;
+
+        public double[] val;
+
+        public GridCell()
+        {
+            p = new Vector3[8];
+            val = new double[8];
+        }
+
+
+    }
+}
+
+   //XYZ p[8];
+   //double val[8];
---
+++ Playground/Metaballs/Metaballs/Metaballs/Triangle.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Triangle.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Metaballs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Microsoft.Xna.Framework;
+
+    //typedef struct
+    //{
+    //    XYZ p[3];
+    //} TRIANGLE;
+
+    public class Triangle
+    {
+        public Vector3[] p;
+
+        public Triangle()
+        {
+            p = new Vector3[3];
+        }
+    }
+}
